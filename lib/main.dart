4352538995a2e94cf7386d95c1e2781e2fad51c4
HDV2k3 <dacviethuynh@gmail.com>
2024-02-19 16:24:00 +0700

    Lesson9

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:ig_ui/pages/edit_profile/widgets_edit_profile/body_profile.dart';
-import 'package:ig_ui/pages/edit_profile/widgets_edit_profile/topbar_profile.dart';
-import 'package:ig_ui/pages/login/widgets_login//body_login2.dart';
-import 'package:ig_ui/pages/login/widgets_login/footer_login2.dart';
-import 'package:ig_ui/pages/login/widgets_login/body_login.dart';
-import 'package:ig_ui/pages/login/widgets_login/footer_login.dart';
-import 'package:ig_ui/pages/login/widgets_login/topbar_login2.dart';
-import 'package:ig_ui/pages/message/widgets_message/footer_direct_message.dart';
-import 'package:ig_ui/pages/message/widgets_message/listchatbox_message.dart';
-import 'package:ig_ui/pages/message/widgets_message/search_direct_message.dart';
+//import 'package:ig_ui/pages/edit_profile/widgets_edit_profile/body_profile.dart';
+//import 'package:ig_ui/pages/edit_profile/widgets_edit_profile/topbar_profile.dart';
+//import 'package:ig_ui/pages/login/widgets_login//body_login2.dart';
+//import 'package:ig_ui/pages/login/widgets_login/footer_login2.dart';
+//import 'package:ig_ui/pages/login/widgets_login/body_login.dart';
+//import 'package:ig_ui/pages/login/widgets_login/footer_login.dart';
+//import 'package:ig_ui/pages/login/widgets_login/topbar_login2.dart';
+//import 'package:ig_ui/pages/message/widgets_message/footer_direct_message.dart';
+//import 'package:ig_ui/pages/message/widgets_message/listchatbox_message.dart';
+//import 'package:ig_ui/pages/message/widgets_message/search_direct_message.dart';
+//import 'package:ig_ui/pages/message/widgets_message/topbar_direct_message.dart';
 import 'package:ig_ui/pages/profile/widgets_profile/BottomNavigationBar.dart';
 import 'package:ig_ui/pages/profile/widgets_profile/appbar_profile.dart';
 import 'package:ig_ui/pages/profile/widgets_profile/body_profile.dart';
-import 'package:ig_ui/pages/message/widgets_message/topbar_direct_message.dart';
 import 'package:ig_ui/pages/home/widgets/bottomNavigationBar.dart';
 
 void main() {
